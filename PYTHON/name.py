5db893a0bba0f99d77df4781e4cea7ba854da60b
Kisu <kisuzoid@gmail.com>
2023-07-27 19:52:06 +0530

    name.py modified

--- PYTHON/name.py
+++ PYTHON/name.py
@@ -2,8 +2,14 @@ name = input("What is your name? ")
 name = name.title().strip()
 print("Hello, ",end = "")
 print(name)
+print("MY, \"friend\" "+"How are you?",name)
 
 # In python documentation, there is end="\n" in print function that makes a new line after the output.
 # Here, using end="" , we modified the print function by erasing "\n" to prevent new line after output.
 # \n is used for new line.
 # \t is used for tab.     
+# similarly, we modified the documentation as pr our need.
+#we can use double quote and single quote both as well.
+#print("Hello, "friend"") this will give syntax error coz double quote is seems to close after friend.
+#to fir that error , print('Hello, "friend"') can be used
+#or use print("MY, \"friend\"")
