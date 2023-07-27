54d92c9c5e4bacf5bd60fa8206fa4728d3d20eda
Kisu <kisuzoid@gmail.com>
2023-07-27 10:57:12 +0530

    hello

--- PYTHON/hello.py
+++ PYTHON/hello.py
@@ -1,2 +1,16 @@
 x = input("What is your name? ")
+x = x.title().strip()
 print("Hello, ",x)
+print("How are you? ")
+
+
+#x is variable here for input() function.
+#input() is a function that takes input.
+#print() is the function for output to the screen.
+#title() is a function that make first letter of every word capital. 
+#strip() is a function that erase whitespaces from both left and right ends.
+#upper() function is for making every word capital.
+#lower() function is for making every word small.
+#lstrip() func. is for removing left end's whitespaces.
+#rstrip() func. is for removing right end's whitespaces.
+#parentheses after function is for add aditional information to perform by functions. 
