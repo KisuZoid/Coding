ca9fd3358143697115f882f811b8019886d42337
Kisu <kisuzoid@gmail.com>
2023-07-28 12:24:27 +0530

    calculator

---
+++ PYTHON/calculator.py
@@ -0,0 +1,34 @@
+x = int(input("x: "))
+y = int(input("y: "))
+
+print(x + y)
+
+
+
+
+"""
+x = input("x: ")
+y = input("y: ")
+
+z = int(x) + int(y)
+
+#int(x) means x is an integer
+
+print("Result:",z)
+"""
+
+
+
+
+
+
+
+
+
+
+#str is class for strings
+#in case of string + is for combining to variables
+#int is for integers
+#in numbers python supports operation like  +,-,%,*
+# + is for add | - is for subtract | * is for multiplication
+# % is modulo which is for taking remiender
