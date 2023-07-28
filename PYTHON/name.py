8c624626546771c6a805e799e7e33f79f1077f8a
Kisu <kisuzoid@gmail.com>
2023-07-28 11:15:59 +0530

    name modified

--- PYTHON/name.py
+++ PYTHON/name.py
@@ -2,7 +2,10 @@ name = input("What is your name? ")
 name = name.title().strip()
 print("Hello, ",end = "")
 print(name)
-print("MY, \"friend\" "+"How are you?",name)
+#or
+print(f"Hello, {name}")
+print("MY, \"friend\" " + "How are you?",name)
+
 
 # In python documentation, there is end="\n" in print function that makes a new line after the output.
 # Here, using end="" , we modified the print function by erasing "\n" to prevent new line after output.
@@ -12,4 +15,6 @@ print("MY, \"friend\" "+"How are you?",name)
 #we can use double quote and single quote both as well.
 #print("Hello, "friend"") this will give syntax error coz double quote is seems to close after friend.
 #to fir that error , print('Hello, "friend"') can be used
-#or use print("MY, \"friend\"")
+#or 
+# use print("MY, \"friend\"") "\" back slace is a scaping character
+#f in line 6, indicate format that whatever in curl bracet is special string
