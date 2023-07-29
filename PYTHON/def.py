39ed8219d10df8eeb3cc669519257bacc245c261
Kisu <kisuzoid@gmail.com>
2023-07-29 12:43:56 +0530

    add

---
+++ PYTHON/def.py
@@ -0,0 +1,37 @@
+def main():
+    name = input("What is your name? ")
+    hello()
+    hello(name)
+
+#main is our main funtion
+
+def hello(to="world"):
+    print("Hello,", to)
+
+main()    
+
+#or
+
+"""def hello(to="world"):
+    print("Hello,", to)
+#default value of to is world, it depend upon us to write default value or not.
+
+name = input("What is your name? ")
+hello()
+hello(name)"""
+
+#here, name variable resembels with parameter "to" of hello() function
+
+#or 
+"""
+def hello():
+    print("hello,")
+
+
+name = input("What is your name? ")
+hello()
+print(name)
+"""
+
+
+#def is for define or creat own function
