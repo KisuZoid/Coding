73203023879679c6ceff5d0d947c333f0490ebef
Kisu <kisuzoid@gmail.com>
2023-07-30 22:43:16 +0530

    modified match

--- PYTHON/cs50/lec_1/house.py
+++ PYTHON/cs50/lec_1/house.py
@@ -1,14 +1,19 @@
 #match keyword
-name = input("What's your name? ")
+name = input("What's your name? ").title().strip()
 
 match name:
     #match variable "name" with all the cases case "Harry" means name associates with Harry there.
-    case "Harry":
+    case "Harry" | "Hermione" | "Ron":
         print("Garud Dwar")
-    case "Hermione":
-        print("Garud Dwar")
-    case "Ron":
-        print("Garud Dwar")
+    
+    #or 
+
+    #case "Harry":
+     #   print("Garud Dwar")
+    #case "Hermione":
+     #   print("Garud Dwar")
+    #case "Ron":
+     #   print("Garud Dwar")
     case "Draco":
         print("Nagsakti")  
     case _:
