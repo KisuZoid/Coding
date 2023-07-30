1f6091738ebe92464f5f2fa3f42880924b203488
Kisu <kisuzoid@gmail.com>
2023-07-30 22:36:06 +0530

    modified match keyword

--- PYTHON/cs50/lec_1/house.py
+++ PYTHON/cs50/lec_1/house.py
@@ -1,3 +1,23 @@
+#match keyword
+name = input("What's your name? ")
+
+match name:
+    #match variable "name" with all the cases case "Harry" means name associates with Harry there.
+    case "Harry":
+        print("Garud Dwar")
+    case "Hermione":
+        print("Garud Dwar")
+    case "Ron":
+        print("Garud Dwar")
+    case "Draco":
+        print("Nagsakti")  
+    case _:
+        # _ (single underscore character) here it is used to denote that cases which is not specified yet
+        print("Who?")  
+
+#or 
+
+"""
 name = input("What's your name? ")
 
 if name == "Harry" or name == "Harmione" or name == "Ron":
@@ -15,3 +35,4 @@ elif name == "Draco":
     print("Nagsakti")
 else :
     print("Who?")
+"""
