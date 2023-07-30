6bb0fcecbb9f6ab25f813ca713b9639e855b0e89
Kisu <kisuzoid@gmail.com>
2023-07-30 20:05:45 +0530

    upgrade

---
+++ PYTHON/cs50/lec_1/condition.py
@@ -0,0 +1,20 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+
+if x > y:
+    print("x is greater than y")
+
+if x == y:
+    print("x is equal to y")
+
+
+
+#syntax :
+# > means greater that
+# >= means greater that equals to
+# <= less than equals to
+# == means equals to
+# != not equals to
