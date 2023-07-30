7b0d1f6e7bfd198d5fdd24f4edae40afc9bda0c1
Kisu <kisuzoid@gmail.com>
2023-07-30 21:50:13 +0530

    init

---
+++ PYTHON/cs50/lec_1/house.py
@@ -0,0 +1,17 @@
+name = input("What's your name? ")
+
+if name == "Harry" or name == "Harmione" or name == "Ron":
+    print("Garud Dwar")
+
+#or
+    
+#if name == "Harry":
+ #   print("Garud Dwar")
+#elif name == "Harmione":
+ #   print("Garud Dwar")
+#elif name == "Ron":
+ #   print("Garud Dwar")
+elif name == "Draco":
+    print("Nagsakti")
+else :
+    print("Who?")
