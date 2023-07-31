c4019c88a0ff5c67e1a2bcda784aca56d32f6137
Kisu <kisuzoid@gmail.com>
2023-07-31 11:41:08 +0530

    modified

--- cs50/X/C/hello.c
+++ cs50/X/C/hello.c
@@ -3,9 +3,9 @@
 
 int main(void)
 {
-    string x=get_string("what is your first name? ");
-    string y=get_string("what is your last name? ");
-    int z=get_int("How much you got in 12th? ");
-    printf("hello, %s %s!\n %i%% is in your 12th\n", x, y, z);
+    string x = get_string("what is your first name? ");
+    string y = get_string("what is your last name? ");
+    float z = get_float("How much you got in 12th? ");
+    printf("Hello, %s %s!\n %.2f%% is in your 12th\n", x, y, z);
 }
 
