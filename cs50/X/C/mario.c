445ad4c97a9b8b60fbc26e8d9ee4d1e340056b69
Kisu <kisuzoid@gmail.com>
2023-07-31 13:40:47 +0530

    modified

--- cs50/X/C/mario.c
+++ cs50/X/C/mario.c
@@ -4,14 +4,13 @@
 int main(void)
 {
     //do while loop
-    int n;//it is a deceleration that there is a integer n.
+    int n; //declare that n is integeral variable
     do
     {
         n=get_int("length? ");
     }
     while (n<1);
-//if value of n typed less than 1 then do  n=get_int("length? "); func. once again.
-
+//if value of n typed less than 1 then do  n=get_int("length? "); function once again.
     int m;
     do
     {
@@ -28,3 +27,43 @@ int main(void)
          printf("\n");
     }
 }
+
+/*
+both are same:
+
+int n=get_int("length? ");
+    while(n<1)
+    {
+        n=get_int("length? ");
+    }
+    
+
+int n;
+    do
+    {
+        n=get_int("length? ");
+    }
+    while (n<1)
+*/
+
+/*
+int main(void)
+{
+    const int n=3;
+
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<4; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+
+    output : ####
+             ####
+             ####
+
+//const means constant, const int n = 3; means we fix the value of n as 3, no matter what further the value of n will be assign in program set(parentheses), n always be 3 for that program set.
+}
+*/
