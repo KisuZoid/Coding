3ef0e7c199960dc52fd74b8624f87fe8d8e2a79c
Kisu <kisuzoid@gmail.com>
2023-08-01 11:51:15 +0530

    modified

--- cs50/X/C/pseudo.c
+++ cs50/X/C/pseudo.c
@@ -26,7 +26,7 @@ int get_size(void)
         n = get_int("size: ");
     }
     while(n<1);
-    return n;
+    return n; //return shows returning the value to the function.
 }
 
 void print_grid(int size)
@@ -41,3 +41,21 @@ void print_grid(int size)
     }
 }
 
+/*
+int get_size(void) 
+
+here "int" represents that this is a integer function i.e. return is in integer.
+as function do to not take any input so,we right keyword in side parentheses "void" that itself meaning void i.e nothing. this function only perform that is mentioned in "{ }"
+
+and functionality of the function is in parentheses"{ }".
+*/
+
+/*
+void print_grid(int size)
+
+here it is just a print function that print whatever be the input(int/string/float etc.). it has no return value(not return any value to the function) so, we write void. 
+void here represents the function type i.e it returns noting.
+
+(int size) here int represents taken input type for the function i.e. integer type input and size is the name of variable of that input.
+and functionality of the function is mentioned in "{ }"
+*/
