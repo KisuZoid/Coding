c38c734d8fb8b8363c32d8744f5a101da37996dd
Kisu <kisuzoid@gmail.com>
2023-08-01 21:14:05 +0530

    initial

---
+++ cs50/Py/lec2/cat.py
@@ -0,0 +1,7 @@
+#while loop
+i = 3
+while i != 0:
+    print("meow")
+    i = i - 1
+#initial value of i is 3, while i != 0: print("meow") means print meow till i not equals to zero.
+#i = i - 1 is a condition for each loop that for every loop value of i decreases by 1, and loop will stop when value of i be 0.
