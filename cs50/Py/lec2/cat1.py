10356b88f496f4b4d1d7ea73a5b5a3b847634184
Kisu <kisuzoid@gmail.com>
2023-08-03 22:06:28 +0530

    modified

--- cs50/Py/lec2/cat1.py
+++ cs50/Py/lec2/cat1.py
@@ -1,6 +1,14 @@
 def main():
-    meow(3)
+    number = get_number()
+    meow(number)
 
+def get_number():
+    while True:
+        n = int(input("what is n? "))
+        if n>0:
+            break
+    return n
+    
 def meow(n):
     for _ in range(n):
         print("meow")
