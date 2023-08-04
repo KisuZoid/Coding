5018fdd96d6f2a92e775450a97f1bc0d31f5fede
Kisu <kisuzoid@gmail.com>
2023-08-05 01:41:47 +0530

    initial

---
+++ cs50/X/lec1_2/command.c
@@ -0,0 +1,30 @@
+//this is how we use command line input direct into our code.
+#include <stdio.h>
+#include "cs50.h"
+
+int main(int argc, string argv[])
+{
+    if (argc == 3) //== means equals to 
+    //argc is equal to 3 means argument count is 3 , one for argv[1], one for argv[2], and one for world 
+    {
+        printf("Hello, %s %s\n", argv[1], argv[2]);
+    } 
+    else
+    {
+        printf("Hello, world");
+    }
+}
+
+
+
+/*
+//int main(void)  as in main() by using void, it is declared that it does not take any command line arguments.
+
+int main(int argc, string argv[])//now main will take command line argument anyhow.
+{
+    printf("hello, %s %s\n", argv[1], argv[2]);
+}
+
+//int argc means argument count is in integer format
+//string argv[] it is array of string that we type in prompt or command line.
+*/
