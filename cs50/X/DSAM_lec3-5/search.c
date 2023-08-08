2a20586d2e900556d0da2c05496a0e8d0d9558bd
Kisu <kisuzoid@gmail.com>
2023-08-09 00:50:37 +0530

    complete

--- cs50/X/DSAM_lec3-5/search.c
+++ cs50/X/DSAM_lec3-5/search.c
@@ -1,25 +1,47 @@
 //1
 #include <stdio.h>
 #include "cs50.h"
+#include <string.h>
 
-int main(void)
+int main(void) //linear search
+{
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
+
+    string s = get_string("String: ");
+    for (int i = 0; i<6; i++)
+    {
+        if (strcmp(strings[i], s) == 0) // means does the return value of a function strcmp(stands for string compare) is 0 when pass in current string and that the user input.
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
+
+
+
+
+/*
+int main(void) //linear search
 {
     int numbers[] = {20, 500, 10, 5, 100, 1, 50}; //static array, array that never changes.
     //means give an array whatever the size of bracket and containing the values in it.
 
     //or 
-    /*
-    int number[7];
-    numbers[0] = 20;
-    numbers[1] = 500;
-    numbers[2] = 10;
-    numbers[3] = 5;
-    numbers[4] = 100;
-    numbers[5] = 1;
-    numbers[6] = 50;
-    */
+    
+    #int number[7];
+    #numbers[0] = 20;
+    #numbers[1] = 500;
+    #numbers[2] = 10;
+    #numbers[3] = 5;
+    #numbers[4] = 100;
+    #numbers[5] = 1;
+    #numbers[6] = 50;
+    
 
-   int n = get_int("Number: "); //linear search
+   int n = get_int("Number: "); 
    for (int i = 0; i<7; i++)
         {
             if (numbers[i]==n)
@@ -31,3 +53,4 @@ int main(void)
         printf("Not found\n");
         return 1;
 }
+*/
