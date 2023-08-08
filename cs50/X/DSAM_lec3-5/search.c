04c8dff0db6f73af9f0e65d6f16b170a4fd5f492
Kisu <kisuzoid@gmail.com>
2023-08-09 00:27:11 +0530

    modified

--- cs50/X/DSAM_lec3-5/search.c
+++ cs50/X/DSAM_lec3-5/search.c
@@ -4,5 +4,30 @@
 
 int main(void)
 {
-    int nimbers[] = {20, 500, 10, 5, 100, 1, 50};
+    int numbers[] = {20, 500, 10, 5, 100, 1, 50}; //static array, array that never changes.
+    //means give an array whatever the size of bracket and containing the values in it.
+
+    //or 
+    /*
+    int number[7];
+    numbers[0] = 20;
+    numbers[1] = 500;
+    numbers[2] = 10;
+    numbers[3] = 5;
+    numbers[4] = 100;
+    numbers[5] = 1;
+    numbers[6] = 50;
+    */
+
+   int n = get_int("Number: "); //linear search
+   for (int i = 0; i<7; i++)
+        {
+            if (numbers[i]==n)
+            {
+                printf("Found\n");
+                return 0;
+            }
+        }
+        printf("Not found\n");
+        return 1;
 }
