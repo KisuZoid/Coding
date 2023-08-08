817e57a91e011ddd6a8d29fffd5097491204b254
Kisu <kisuzoid@gmail.com>
2023-08-08 16:56:33 +0530

    update

--- cs50/Py/lec2/hogwart.py
+++ cs50/Py/lec2/hogwart.py
@@ -1,10 +1,18 @@
 #List(count starts from 0)
 
+students = ["Hermione", "Harry", "Ron"]
+
+for s in students:
+    print(s) # s is a variable denotes values of students variable. and in python it automatically itterate hermione 1st and continue for next and next values as well. 
+
+
+"""
 students = ["Hermione", "Harry", "Ron"] #students is a variable for list of 3 student.
 #if we want to show particular student let say Hermione so use variable name and position of hermione in sq. bracket. i.e. 0 in this case. 
 
 print(students[0])
-
+print(students)
+"""
 
 
 
