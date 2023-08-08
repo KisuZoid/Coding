af6c428a6c4f91e88809982740a12ac27ef635e2
Kisu <kisuzoid@gmail.com>
2023-08-09 01:24:27 +0530

    modified

---
+++ cs50/X/DSAM_lec3-5/phonebook.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "cs50.h"
+#include <string.h>
+
+int main(void)
+{
+    string names[] = {"Kislay", "Kisu"};
+    string numbers[] = {"+911234567890", "+911029384756"};
+
+    string name = get_string("Name: ");
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
