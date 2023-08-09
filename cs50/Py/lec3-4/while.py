efe2d6c6bf584ef1b90429ce8f13fda8de3f21ec
Kisu <kisuzoid@gmail.com>
2023-08-09 13:44:25 +0530

    initial

--- cs50/Py/lec3-4/while.py
+++ cs50/Py/lec3-4/while.py
@@ -1,1 +1,12 @@
 #2
+while True:
+    try:
+        x = int(input("What is x? "))
+    except ValueError:
+        print("x is not an integer.\n")
+    else:
+        break
+
+print(f"x is {x}")
+
+#while True is for infinite loop till condition is True
