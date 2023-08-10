e2522a1d40e42869cb2e9f83a9bd5d1b90148e4e
Kisu <kisuzoid@gmail.com>
2023-08-11 00:14:33 +0530

    modified

---
+++ cs50/Py/lec5/test_calculator.py
@@ -0,0 +1,21 @@
+#1
+from calculator import square
+
+def main():
+    test_square()
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    #or
+    '''
+    if square(2) != 4:
+        print("2 squared was not 4")
+    if square(3) != 9:
+        print("3 squared was not 9")
+    '''
+
+if __name__ == "__main__":
+    main()
+
+# assert keyword is allows to assert something is true. and if it is originally true nothing is going to happen but is originally false then seeing some kind of error generally AssertionError. 
