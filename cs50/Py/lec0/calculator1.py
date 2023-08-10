252fbb7882c89a2c4a38a9f1d1d1b13dab5489e9
Kisu <kisuzoid@gmail.com>
2023-08-10 20:30:17 +0530

    modified

--- cs50/Py/lec0/calculator1.py
+++ cs50/Py/lec0/calculator1.py
@@ -8,7 +8,8 @@ def square(n):
 
 #return keyword returns a value to  the function
 
-main()
+if __name__ == "__main__": #explained in lec3-4 saying.py
+    main()
 
 
 #return n*n | return n ** 2 | return pow(n, 2) here n is no. and 2 indicate power
