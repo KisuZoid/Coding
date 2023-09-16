df57a39d104546cea8243eeaf81b1ee86e09f870
Kisu <kisuzoid@gmail.com>
2023-09-16 09:53:42 +0530

    modified

--- cs50/Py/lec3-4/9cow.py
+++ cs50/Py/lec3-4/9cow.py
@@ -11,3 +11,4 @@ import sys
 if len(sys.argv) == 2:
     cowsay.cow("hello, " + sys.argv[1]) #cowsay.cow program gives output said by cow ;)
     cowsay.trex("hello, " + sys.argv[1]) #cowsay.trex program gives output said by trex :)
+    cowsay.dragon("hello, " + sys.argv[1])
