e77275ce71e39b9629af8b2bd4e34906c8bcf579
Kisu <kisuzoid@gmail.com>
2023-11-27 22:47:21 +0530

    day 2

---
+++ PYTHON/Udemy/day_2/data_type_1.py
@@ -0,0 +1,24 @@
+#string
+
+print("Hello"[0])
+print("123" + "456" + " Kisu")
+
+user_name = "Kisu Zoid"
+print(user_name[0] + user_name[5])
+
+#integer
+
+print(123 + 456)
+
+
+# [] represents array and count of an array starts with 0(zero)
+# print(len(1234)) -> gives error(typeError) object of type "int" as no len()
+
+
+"""
+    Basic Data_Type:
+        string  : array of characters
+        integer : represents integral numbers
+        float   : number with decimal value
+        boolean : true(1) or false(0) value 
+"""
