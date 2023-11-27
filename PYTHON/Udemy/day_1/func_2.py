e6c92825ae42176813242a3bc74103722fd53f73
Kisu <kisuzoid@gmail.com>
2023-11-27 22:24:59 +0530

    modified day 2

--- PYTHON/Udemy/day_1/func_2.py
+++ PYTHON/Udemy/day_1/func_2.py
@@ -9,7 +9,7 @@ print("Number of character in a input string : "+str(length_input_string))
 
 """
     input() function allow user to give the input.
-    len() function gives the lenght of the sting, i.e. number of character in a string | it even count whitespaces as well. 
+    len() function gives the lenght of the sting, i.e. number of character in a string written between quotes inside len() func. | it even count whitespaces as well. 
         as len() gives integeral output, so we have to force python to treat it as string so that it will concatenate using + symbol.
         int() represent integer datatype
         str() represent string datatype
