f254564d46b0098c26b821f41802cb7967143f52
Kisu <kisuzoid@gmail.com>
2024-01-13 17:17:48 +0530

    add

---
+++ JAVA/_5_Arithmetic.java
@@ -0,0 +1,58 @@
+public class _5_Arithmetic {
+    public static void main(String[] args) {
+        // expression = operands & operators
+        // operands = values, variables, numbers, quantity
+        // arithmatic operator = + - * / % A++ ++A A-- --A
+
+        System.out.println(3 + 4); // Direct use without declaration
+
+        int num_1, num_2, add, sub; // Declare then Assigning the value
+        num_1 = 3;
+        num_2 = 4;
+        add = num_1 + num_2;
+        sub = num_2 - num_1;
+        System.out.println(num_1 + "+" + num_2 + " = " + add); //Addition
+        System.out.println(num_2 + "-" + num_1 + " = " + sub); //Subtraction
+
+        int post;
+
+        post = 5;
+        System.out.println("\nBefore increment(x) = " + post);
+        System.out.println("At increment(x++) = " + post++);
+        System.out.println("After post increment(x) = " + post);
+
+        post = 5;
+        System.out.println("\nBefore decrement(x) = " + post);
+        System.out.println("At decrement(x--) = " + post--);
+        System.out.println("After post decrement(x) = " + post);
+
+        int pre;
+
+        pre = 5;
+        System.out.println("\nBefore increment(x) = " + pre);
+        System.out.println("At increment(++x) = " + ++pre);
+        System.out.println("After pre increment(x) = " + pre);
+
+        pre = 5;
+        System.out.println("\nBefore decrement(x) = " + pre);
+        System.out.println("At decrement(--x) = " + --pre);
+        System.out.println("After post decrement(x) = " + pre + "\n");
+
+        int num_3 = 2;
+        System.out.println(4 + "/" + num_3 + " = " + 4/num_3); //Division
+        System.out.println(num_2 + "%" + num_1 + " = " + num_2%num_1); //Modulo, A%B : Gives reminder when we divide A to B.
+        System.out.println(num_1 + "*" + num_2 + " = " + num_1*num_2); //Multiplication
+    }
+}
+
+/* #Code Explain:- (atithmetic operator)
+ * + : Addition
+ * - : subtraction
+ * * : multiplication
+ * / : division
+ * % : returns the division reminder
+ * ++A => increase value of A by 1, then assign it to A. i.e. ++8 = 9.
+ * A++ => define A then increase value of A by 1. i.e. 8++ = 8 and new value will become 9 for next time.
+ * --A => decrease value of A by 1, then assign it to A. i.e. --8 = 7.
+ * A-- => define A then decrease value of A by 1. i.e. 8-- = 8 and new value will become 7 for next time.
+ */
---
+++ JAVA/_6_Operator.java
@@ -0,0 +1,70 @@
+public class _6_Operator {
+    public static void main(String[] args){
+        // Assignment Operators
+        int x = 10;
+        System.out.println("x = " + x);
+        x += 5;
+        System.out.println("10 + 5 = " + x + "\n");
+
+        int y;
+
+        y = 10;
+        System.out.println("y = " + y);
+        y+=3;
+        System.out.println("y+=3 => y=y+3 i.e. "+ y);
+
+        y = 10;
+        System.out.println("y = " + y);
+        y-=3;
+        System.out.println("y-=3 => y=y-3 i.e. "+ y);
+
+        y = 10;
+        System.out.println("y = " + y);
+        y*=3;
+        System.out.println("y*=3 => y=y*3 i.e. "+ y);
+
+        double z = 10;
+        System.out.println("y = " + z);
+        z/=3;
+        System.out.println("y/=3 => y=y/3 i.e. "+ z);
+
+        y = 10;
+        System.out.println("y = " + y);
+        y%=3;
+        System.out.println("y%=3 => y=y%3 i.e. "+ y);
+
+    }
+}
+
+/* Assignment operator:-
+ * = : Assignment operator : assign left side value to the right side value.
+ * += : x+=3 : same as x=x+3
+ * -= : x-=3 : same as x=x-3
+ * *= : x*=3 : same as x=x*3
+ * /= : x/=3 : same as x=x/3
+ * %= : x%=3 : same as x=x%3
+ * &= : x&=3 : same as x=x&3
+ * |= : x|=3 : same as x=x|3
+ * ^= : x^=3 : same as x=x^3
+ * >>= : x>>=3 : same as x=x>>3
+ * <<= : x<<=3 : same as x=x<<3
+ */
+
+/* Comparision Operator:- (Return true, if condition will be true else return false)
+ * == : equal to : x=y (x equals to y)
+ * != : not equals to : x!=y (x not equals to y)
+ * > : greater than : x>y (x greater than y)
+ * >= : greater than equals to : x>=y (x greater than equals to y)
+ * < : less than : x<y (x less than y)
+ * <= : less than equals to : x<=y (x less than equals to y)
+ */
+
+/* Logical Operator:-
+ * && : Logical And : return true if both satatement are true : x<5 && x<10
+ * || : Logical Or : return true if any one of the statements is true : x<5 || x<4
+ * ! : Logical Not : reverse the result, return false if the result is true and vice versa : !(x<5 && x<10)
+ */
+
+
+
+
