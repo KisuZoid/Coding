7cc73db76ec37f4cd4f245216f1b2cec195158c1
Kisu <kisuzoid@gmail.com>
2024-02-12 16:54:45 +0530

    add

---
+++ CPP/_01_Basic.cpp
@@ -0,0 +1,43 @@
+//Header file
+#include <iostream>
+
+//Executable Code block should be inside main() function. 
+int main(){
+    //print Hello World
+    std::cout << "Hello World";
+    return 0;
+}
+
+/*
+Multi 
+line 
+comment
+*/
+
+//Single line comment
+
+/*
+# Header files is used for inhance the program functionality.
+    #include <iostream> : include the iostream(input output stream) file to the program.
+                            that helps in input(cin), output(cout) and other tasks as well.
+    
+    Function:
+        <returnType> <functionName>(parentheses){
+            //code block or functionality
+        }
+
+        int main(){...} : main() function return integer value to the Operating System(O.S).
+                            return 0; means returns 0 to O.S means program is success full, else failed.
+                            functionality of main() function will be inside curly braces {}.
+    
+    : "cout" is the standared output stream, which lies inside standared namespace ("std").
+         std::cout means call cout from std.
+         ";" is used for terminate the statement.
+        
+    : If we not want to use "std" before every "std-file" then just use:
+        "using namespace std;" after header file.
+        or 
+        "using std::cout;" after header file, call cout from std which is applicable for all cout function. 
+    
+    : string should be written inside double quote. "String"
+*/
---
+++ CPP/_02_Variable.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //variable declaration
+    string name;
+    int age;
+    
+    //Output What is your name? to the screen.
+    cout << "What is your name? ";
+    // Input from user and assigned to name variable.
+    cin >> name;
+
+    cout << "What is your age? ";
+    cin >> age;
+
+    cout << "Your name is " << name 
+         << " and you are " 
+         << age << " years old.";
+
+    return 0;
+}
+
+/*
+# Variable is just a container for the particular value.
+ex: Primary we have 5 data types:
+    1. "int" for intergral data type.
+    2. "float" for floating point values of low precision.
+    3. "double" for storing larger float value(high precision).
+    4. "bool" for boolean values(True(1) or False(0))
+    5. "char" for single character. 'A' : written inside single quotes.
+
+//Combination of "char" is "string"
+    "string" for string data type. "Kisu" : written inside double quotes.
+
+Syntax:
+//Declaration of variable
+    dataType variableName;
+
+//Initializing the value
+    variableName = value;
+
+--or--
+
+//Declaration and Initializing at same time
+    dataType variableName = value;
+
+-> ex:
+        int num = 5; // means assign 5 to variable "num" which contains integral values.
+        string name = "Kisu"; // means assign Kisu to variable "name" which contain string of characters.
+
+# Variable Scope:
+    : A variable region in a program where the existence of that variable is valid.
+
+    -> Local variables:
+        : it declared inside the braces of any function and can be accessed only from that particular function.
+
+    -> Global Variable:
+        :  it is declared outside any function and can be accessed from everywhere.
+
+    *Name of local and global variable can be same, but value of variable will determined by Local value.
+
+# Data Type: (3 types)
+    1. Built-in : int, float, double, bool, char ...
+    2. User-defined : Class, Struct, Union, Enum ...
+    3. Derived: Array, Function, Pointer ...
+
+#Naming of Variable:
+    1. Range 1-255 characters.
+    2. Must begin with a letter of the alphabet or an underscore(_).
+    3. It contain letters and numbers.
+    4. Case sensitive.
+    5. No spaces and special charcaters are allowed.
+    6. Can't use reserved keyword(variable name inside code documentation).
+    
+    Note: Name should be clear and describing its functionality.
+        ex: //variable for age
+            int a; -> wrong approach
+            int age; -> correct approach
+*/
