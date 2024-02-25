5ebf98cb905512db603811052ef3aa2eec60927e
Kisu <kisuzoid@gmail.com>
2024-02-25 23:26:33 +0530

    upgrade

--- CPP/_05_Manipulator.cpp
+++ CPP/_05_Manipulator.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main(){
+    //Constants in c++ :- value can't be change further inside the code.
+    const int a = 3; //a=3 is fixed for the program.
     
     return 0;
 }
