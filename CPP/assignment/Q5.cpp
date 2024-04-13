bb714110b94b117637829e70da2dde940fa21fb4
Kisu <kisuzoid@gmail.com>
2024-04-14 04:30:23 +0530

    add

---
+++ CPP/assignment/Q5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int num[]= {1, 2, 3}; //--> 3 * 4
+    cout<<sizeof(num); //sizeof() is used for find the size of array * bit per array. --> int => 4, char => 1, double => 8, float => 4.
+    return 0;
+}
