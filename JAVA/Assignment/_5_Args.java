639981114ef37a63060c2d1f2cdae80fcb1816f6
Kisu <kisuzoid@gmail.com>
2024-05-03 22:43:16 +0530

    add

---
+++ JAVA/Assignment/_5_Args.java
@@ -0,0 +1,8 @@
+package Assignment;
+
+public class _5_Args{
+    public static void main(String[] args)
+    {
+        System.out.println(args[0]); //--> i.e. when we pass argument, it will gonna store in array of string "String[] args" and as we command args[0] to print means 0th argument will gonna print
+    }
+}
