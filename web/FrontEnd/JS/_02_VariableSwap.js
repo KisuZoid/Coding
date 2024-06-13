b9fca8e35ccef007f68ca54111920c3f98ec88b1
Kisu <kisuzoid@gmail.com>
2024-06-13 17:57:55 +0530

    js basic

---
+++ web/FrontEnd/JS/_02_VariableSwap.js
@@ -0,0 +1,13 @@
+var a, b;
+a = 8;
+b = 4;
+console.log("Value of 'a' is " + a + " And Value of 'b' is " + b);
+
+console.log(); //for spacing, we can use this empty print or "\n"
+
+var temp = a;
+a = b;
+b = temp;
+console.log("Swapped Value of 'a' is " + a + " And swapped Value of 'b' is " + b + "\n"); //if we use double quote for a and b, then in results, first quote will end the main quote. --> i.e. generate error || so, for using that, we have to use space character i.e. \(back-slash)
+console.log("\"Kisu\" \n");
+console.log('\t \'Kisu\''); // "\t" use to get tab.
