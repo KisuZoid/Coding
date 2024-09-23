354c3e1befdd08b81118876193e33b2f0d3ee62e
Kisu <kisuzoid@gmail.com>
2024-09-23 14:12:34 +0530

    additional swap method without extra variable

--- web/FrontEnd/JS/_02_VariableSwap.js
+++ web/FrontEnd/JS/_02_VariableSwap.js
@@ -9,5 +9,18 @@ var temp = a;
 a = b;
 b = temp;
 console.log("Swapped Value of 'a' is " + a + " And swapped Value of 'b' is " + b + "\n"); //if we use double quote for a and b, then in results, first quote will end the main quote. --> i.e. generate error || so, for using that, we have to use space character i.e. \(back-slash)
+
+//Swap without additional variable
+var a, b;
+a = 3;
+b = 8;
+console.log(a, " " , b + " \n ");
+
+a = a+b;
+b = a-b;
+a = a-b;
+console.log(a, " " , b + " \n ");
+
+
 console.log("\"Kisu\" \n");
 console.log('\t \'Kisu\''); // "\t" use to get tab.
