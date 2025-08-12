ec0d3dbd79162a762783e7efae70ff6e3ca40698
Kisu <kisuzoid@gmail.com>
2025-08-13 04:05:36 +0530

    update

--- JAVA/_007_Math.java
+++ JAVA/_007_Math.java
@@ -29,5 +29,17 @@ public class _007_Math {
         System.out.println("Rounding Up of " + x + " = " + roundUp);
         System.out.println("Rounding Down of " + x + " = " + roundDown);
 
+        //printf() => is a method used to format output
+        // %[flags][width][.precision][specifier-character] --> add %[one of those mentioned character] to format output --> % is the placeholder where the value will be printed.
+        // %d --> integer, %f --> float, %s --> string, %c --> character, %b --> binary
+        // %.2f --> 2 decimal places
+
+        String name = "Rahul";
+        char firstLetter = 'R';
+        int age = 25;
+        double height = 60.8;
+        boolean isEmployed = true;
+        System.out.printf("My name is %s, my first letter is %c, my age is %d, my height is %.2f and I am %b\n", name, firstLetter , age, height, isEmployed); //--> \n is for new line.
+        //output: My name is Rahul, my first letter is R, my age is 25, my height is 60.80 and I am true
     }
 }
