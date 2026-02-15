edade42d68cef0da73ad6fcedb061c83888befd7
Kisu <kisuzoid@gmail.com>
2026-02-16 01:36:49 +0530

    update typo in file name and add new file for gauss elimination method

--- college/MAT/module_1/Newton_Raphson.py
+++ college/MAT/module_1/Newton_Raphson.py
@@ -1,25 +1,31 @@
+# x^3−4x+9=0
+
+#function
+def f(x):
+    return x**3 - 4*x + 9
+
+#differentiation of f(x)
+def df(x):
+    return 3*x**2 - 4
+
 """
-27x+5y-3z=45 
-x-2y+7z=70 
-6x+15y+z=29
+f(-3) < 0 and f(-2) > 0 -> root lies between -3 and -2
+
+average= ((-3)+(-2))/2 => -2.5 <- initial guessed solution
+
+Newton - Raphson formula:
+x"n+1" = x"n" - f(x"n")/f'(x"n") -> f'() => df()
 """
 
-import numpy as np
+x0 = -2.5 #initial guess
+tolerance = 1e-6 #Stop when the error is smaller than this value i.e error<0.000001
+max_iter = 20 #max repetition of the numerical formula
 
-#coefficiet matrix
-A = np.array([
-    [27,5,-3],
-    [1,-2,7],
-    [6,15,1]
-], dtype=float)
+x = x0
+for i in range(max_iter):
+    x_new = x - f(x)/df(x)
+    if abs(x_new - x) < tolerance:
+        break
+    x = x_new
 
-#constant vector
-B = np.array([45,70,29], dtype=float)
-
-#solve using gauss elimination
-solution = np.linalg.solve(A, B)
-
-print("x = ", solution[0])
-print("y = ", solution[1])
-print("z = ", solution[2])
-
+print("Root = ", x)
